6fd75b468be89704f9bdc601115a95c4df222e50
tycao <616881845@qq.com>
2017-11-18 18:34:44 +0800

    【C++标准程序库 ：自修教程与参考手册】std_Vector

--- Cpp_STL/function_object/std_vector/main.cpp
+++ Cpp_STL/function_object/std_vector/main.cpp
@@ -1,8 +1,57 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <iterator>
+
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    // create empty vector for strings
+    std::vector<std::string> sentence;
 
-    return a.exec();
+    // reserve memory for five elements to avoid reallocation
+    sentence.reserve(5);
+
+    // append some elements
+    sentence.push_back("Hello, ");
+    sentence.push_back("how");
+    sentence.push_back("are");
+    sentence.push_back("you");
+    sentence.push_back("?");
+
+    // print elements separated with spaces
+    std::copy(sentence.begin(), sentence.end(),
+              std::ostream_iterator<std::string>(std::cout, " "));
+    std::cout << std::endl;
+
+    // print "technical data"
+    std::cout << "max_size() : " << sentence.max_size() << std::endl;
+    std::cout << "size() : " << sentence.size() << std::endl;
+    std::cout << "capacity() : " << sentence.capacity() << std::endl;
+
+    // swap the 2nd and 4th element
+    std::swap(sentence[1], sentence[3]);
+
+    // insert element "always" before "?"
+    sentence.insert(std::find(sentence.begin(), sentence.end(), "?"), "always");
+
+    std::copy(sentence.cbegin(), sentence.cend(),
+              std::ostream_iterator<std::string>(std::cout, " "));
+    std::cout << std::endl;
+
+    // replace "!" with the last element
+    sentence.back() = "!";
+
+    // print elements separated with spaces
+    std::copy(sentence.cbegin(), sentence.cend(),
+              std::ostream_iterator<std::string>(std::cout, " "));
+    std::cout << std::endl;
+
+    // print "technical data"
+    std::cout << "max_size() : " << sentence.max_size() << std::endl;
+    std::cout << "size() : " << sentence.size() << std::endl;
+    std::cout << "capacity() : " << sentence.capacity() << std::endl;
+
+    return 0;
 }
