d187f2bfe6fcee1515d6a29fee3ff5a309af2526
tycao <616881845@qq.com>
2017-11-26 22:16:42 +0800

    C++标准程序库：自修教程与参考手册 ：iostream 迭代器

--- Cpp_STL/Stream_Iterator/iostream/main.cpp
+++ Cpp_STL/Stream_Iterator/iostream/main.cpp
@@ -1,8 +1,26 @@
-#include <QCoreApplication>
+#include <iostream>
+#include <string>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    istream_iterator<string> cinPos(cin);
+    ostream_iterator<string> coutPos(cout, " ");
 
-    return a.exec();
+    // while input is not at the end of the file, write every third string
+    while (cinPos != istream_iterator<string>())
+    {
+        // ignore the following two strings
+        advance(cinPos, 2);
+
+        // read and write the third string
+        if (cinPos != istream_iterator<string>())
+            *coutPos++ = *cinPos++;
+    }
+    cout << endl;
+
+    return 0;
 }
